5341fa1ad5afb932371796319e756e7f9bcd553d
pya-h <p4ya.gcc@gmail.com>
2024-11-01 03:57:59 +0330

    [impl] per gambler stats & function docs

--- blockchain/src/PredictionMarket.sol
+++ blockchain/src/PredictionMarket.sol
@@ -2,7 +2,7 @@ pragma solidity 0.8.28;
 
 contract PredictionMarket {
     enum Outcome {
-        Alpha,
+        Alpha, // [Based on th question alpha & beta will be shown with a specific title in front (or returned from backend or whatever.])
         Beta
     }
     uint256 idOffset = 0;
@@ -19,6 +19,12 @@ contract PredictionMarket {
         uint256 totalAmount;
     }
 
+    struct GamblerBetsStatus {
+        address gambler;
+        uint256 count;
+        uint256 totalAmount;
+    }
+
     address oracle;
     uint256 public finishedAt; // epoch time
     mapping(Outcome => uint8) public trueness;
@@ -29,6 +35,10 @@ contract PredictionMarket {
         oracle = _oracleAddress;
     }
 
+    /**
+     * Allows a gambler to bet specific amount of ethers on an outcome
+     * @param choice: Outcome.Alpha | Outcome.Beta
+     */
     function betOn(Outcome choice) external payable {
         require(finishedAt > 0, "Market closed.");
         bets[choice].push(Bet(++idOffset, msg.sender, msg.value, choice));
@@ -39,6 +49,10 @@ contract PredictionMarket {
         }
     }
 
+    /**
+     * Gives general statistics regrading a specific outcome
+     * @param outcome: Outcome.Alpha | Outcome.Beta
+     */
     function outcomeCurrentStatistics(
         Outcome outcome
     ) external view returns (OutcomeBetStats memory) {
@@ -48,6 +62,42 @@ contract PredictionMarket {
         for (uint256 i = 0; i < bets[outcome].length; i++) {
             totalAmount += bets[outcome][i].amount;
         }
+        return stats;
+    }
+
+    /**
+     * This function returns the statistics regarding an specific gambler (user)
+     * @param gambler : wallet address of the user getting its stats.
+     */
+    function gamblerCurrentStatistics(
+        address gambler
+    ) external view returns (GamblerBetsStatus memory) {
+        GamblerBetsStatus memory stats = GamblerBetsStatus(
+            gambler,
+            0,
+            investmentsPerGamblers[gambler]
+        );
+
+        (uint256 minLength, Outcome popular) = bets[Outcome.Alpha].length <=
+            bets[Outcome.Beta].length
+            ? (bets[Outcome.Alpha].length, Outcome.Beta)
+            : (bets[Outcome.Beta].length, Outcome.Alpha);
+
+        for (uint256 i = 0; i < minLength; i++) {
+            if (bets[Outcome.Alpha][i].gambler == gambler) {
+                stats.count++;
+            }
+            if (bets[Outcome.Beta][i].gambler == gambler) {
+                stats.count++;
+            }
+        }
+
+        for (uint256 i = minLength; i < bets[popular].length; i++) {
+            if (bets[popular][i].gambler == gambler) {
+                stats.count++;
+            }
+        }
+
         return stats;
     }
 }
